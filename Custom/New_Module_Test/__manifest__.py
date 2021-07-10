65025b8a490d2cf0c328a45f45bad08669588394
mohammed.alsumat <mohammed.alsumat@moore-yemen.com>
2021-07-10 18:51:27 +0300

    Editing the two mandatory files(init, manifest)

---
+++ Custom/New_Module_Test/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Module Test',
+    'version' : '1.0',
+    'summary': 'Testing new module',
+    'sequence': 10,
+    'description': """""",
+    'category': 'Productivity',
+    'website': 'https://www.moore-yemen.com',
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
